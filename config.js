a1d487fe12501fc565031abfc245cc89b6bf9a80
gourav-sharma1857 <whogourav@gmail.com>
2026-06-08 16:04:52 -0500

    deploy: move config.js directly to repo for easy hosting

---
+++ config.js
@@ -0,0 +1,12 @@
+function getConfig() {
+  return {
+   apiKey:             "AIzaSyBDJC6miE4wgn7DTfHnZWeaplDwVllDD-U",
+  authDomain:        "planner-b4861.firebaseapp.com",
+  projectId:         "planner-b4861",
+  storageBucket:     "planner-b4861.firebasestorage.app",
+  messagingSenderId: "50924049187",
+  appId:              "1:50924049187:web:98beddf236cbf143d0f296"
+};
+}
+
+window.__FIREBASE_CONFIG = getConfig();
